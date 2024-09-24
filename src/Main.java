e86f972749f6b407070c52dfcd06f410eb51398d
S0LARANA <s0169513@edu.kubsu.ru>
2024-09-24 23:24:11 +0300

    [lab 1] add cyrillic prints in console (why?)

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,27 @@
 import mpi.*;
 
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) throws MPIException {
         MPI.Init(args);
 
         int myrank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
-        int[] message = new int[1];
+        String message = "привет";
+        char[] buffer = new char[message.length()];
+        message.getChars(0, message.length(), buffer, 0);
         int TAG = 0;
         Status status;
 
-        message[0] = myrank;
-
         if (myrank % 2 == 0) {
             if (myrank + 1 != size) {
-                MPI.COMM_WORLD.Send(message, 0, 1, MPI.INT, myrank + 1, TAG);
+                MPI.COMM_WORLD.Send(buffer, 0, buffer.length, MPI.CHAR, myrank + 1, TAG);
+                System.out.println(myrank + " отправил сообщение '" + message + "' получателю " + (myrank + 1));
             }
         } else {
-            if (myrank != 0) {
-                MPI.COMM_WORLD.Recv(message, 0, 1, MPI.INT, myrank - 1, TAG);
-                System.out.println("received: " + message[0]);
-            }
+            MPI.COMM_WORLD.Recv(buffer, 0, buffer.length, MPI.CHAR, myrank - 1, TAG);
+            System.out.println(myrank + " получил: '" + message + "' от " + (myrank - 1));
         }
 
         MPI.Finalize();
